8ffc533f83b36712264c844e91857acacc524edb
Jefferson Snow <socialhit@gmail.com>
2017-01-25 14:15:57 -0700

    background images now work

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,39 @@
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
+// var webpack = require('webpack');
+//
+// module.exports = {
+//     entry: {
+//         main: './public/views/client.js'
+//     },
+//     output: {
+//         filename: './public/dist/scripts/[name].js',
+//         publicPath: '/assets/'
+//     },
+//     module: {
+//         loaders: [
+//             {
+//                 test: /\.js$/,
+//                 exclude: /node_modules/,
+//                 loader: 'babel',
+//             },
+//             {
+//                 test: /\.scss/,
+//                 loader: ExtractTextPlugin.extract('css!sass')
+//             },
+//             {
+//                 test: /\.(jpg|png|gif|svg/)$/,
+//                 loader: 'file?name=[name].[ext]'
+//             },
+//         ]
+//     },
+//
+//
+//     plugins: [
+//         new ExtractTextPlugin('public/dist/styles/main.css', {
+//             allChunks: true
+//         })
+//     ]
+// };
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var webpack = require('webpack');
 
@@ -17,28 +53,19 @@ module.exports = {
                 loader: 'babel',
             },
             {
-                test: /\.scss/,
+                test: /\.scss$/,
                 loader: ExtractTextPlugin.extract('css!sass')
-            },
-            // {
-    //    test: /\.scss$/,
-    //    loader: 'style-loader!css-loader!sass-loader'
-    //  },
-            // {test:/\.(jpg|png)$/,loader:"file-loader"},
+            }
+            ,
             {
-                test: /\.(jpg|png|gif|svg/)$/,
+                test: /\.(jpe?g|png|gif|svg)$/,
                 loader: 'file?name=[name].[ext]'
-            },
+                },
         ]
     },
-
-
-    // plugins: [
-    //     new ExtractTextPlugin('public/dist/styles/main.css', {
-    //         allChunks: true
-    //     })
-    // ]
+    plugins: [
+        new ExtractTextPlugin('public/dist/styles/main.css', {
+            allChunks: true
+        })
+    ]
 };
-resolve: {
-     extensions: ["", ".js",]
- }
